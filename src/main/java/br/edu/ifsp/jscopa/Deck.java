7c96fab2bcaea615f5bb3e937869dacdb625c62f
Rogério Castro Chaves <165519999+rogerioccastro@users.noreply.github.com>
2024-11-24 20:15:07 -0300

    Revert "Criado classe Jogo.java (Incompleto) e atualizado classe Deck.java"
    This reverts commit eac323b7320177c2a3b229165c2f324f00186275.

--- src/main/java/br/edu/ifsp/jscopa/Deck.java
+++ src/main/java/br/edu/ifsp/jscopa/Deck.java
@@ -15,18 +15,22 @@ public class Deck {
 
     // metodo inicializador do deck
     public void reset() {
+
         Carta.Naipe[] naipes = Carta.Naipe.values();
         qtdCartasDeck = 0;
 
         // adiciona todas as cartas (de todos os valores (1 a 10) e naipes) no deck  
         for (int i = 0; i < naipes.length - 1; i++) {
+
             Carta.Naipe naipe = naipes[i];
 
             cartas[qtdCartasDeck++] = new Carta(naipe, Carta.Numero.getNumero(0));
 
             for (int j = 1; j < 10; j++) {
+
                 cartas[qtdCartasDeck++] = new Carta(naipe, Carta.Numero.getNumero(j));
                 cartas[qtdCartasDeck++] = new Carta(naipe, Carta.Numero.getNumero(j));
+
             }
 
         }
@@ -38,35 +42,22 @@ public class Deck {
     }
 
     public void embaralhar() {
+
         int n = cartas.length;
 
         Random aleatorio = new Random();
 
         for (int i = 0; i < cartas.length; i++) {
 
-            // pegar um index aleatorio do array ALÉM do index atual
-            // trocar o elemento aleatorio pelo elemento atual
+            //pegar um index aleatorio do array ALÉM do index atual
+            //trocar o elemento aleatorio pelo elemento atual
             int valorAleatorio = i + aleatorio.nextInt(n - 1);
             Carta cartaAleatoria = cartas[valorAleatorio];
             cartas[valorAleatorio] = cartas[i];
             cartas[i] = cartaAleatoria;
+
         }
 
     }
 
-    //metodo que compra cartas de acordo com a quantidade informada
-    public Carta[] comprarCartas(int n) throws IllegalArgumentException {
-
-        if(estaVazio()){
-            throw new IllegalArgumentException("Nao foi possivel comprar cartas porque nao ha cartas no deck");
-        }
-
-        Carta[] retorno = new Carta[n];
-
-        for(int i = 0; i < n; i++){
-            retorno[i] = cartas[--qtdCartasDeck];
-        }
-
-        return retorno;
-    }
 }
