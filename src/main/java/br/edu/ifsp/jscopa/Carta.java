29b91921511fa74e01207ab60fd68534d4a3c307
Rogério Castro Chaves <165519999+rogerioccastro@users.noreply.github.com>
2024-11-23 23:32:33 -0300

    Criado a classe Deck.java e formatado a classe Carta.java

--- src/main/java/br/edu/ifsp/jscopa/Carta.java
+++ src/main/java/br/edu/ifsp/jscopa/Carta.java
@@ -4,66 +4,62 @@
  */
 package br.edu.ifsp.jscopa;
 
-/**
- *
- * @author rogreio
- */
 public class Carta {
-    
-    enum Naipe{
-    
+
+    enum Naipe {
+
         Paus, Ouros, Copas, Espadas;
-        
+
         private static final Naipe[] naipes = Naipe.values();
-        public static Naipe getNaipes(int i){
-        
+
+        public static Naipe getNaipes(int i) {
+
             return Naipe.naipes[i];
-            
+
         }
     }
 
-    enum Numero{
-    
+    enum Numero {
+
         A, Dois, Tres, Quatro, Cinco, Seis, Sete, Valete, Dama, Rei;
-        
+
         private static final Numero[] numeros = Numero.values();
-        public static Numero getNumero(int i){
-        
+
+        public static Numero getNumero(int i) {
+
             return Numero.numeros[i];
-            
+
         }
     }
-    
+
     private final Naipe naipe;
     private final Numero numero;
-    
-    public Carta(final Naipe naipe, final Numero numero){
-    
+
+    public Carta(final Naipe naipe, final Numero numero) {
+
         this.naipe = naipe;
         this.numero = numero;
-        
+
     }
-    
+
     //metodos getters
-    
-    public Naipe getNaipe(){
-    
+    public Naipe getNaipe() {
+
         return this.naipe;
-    
+
     }
-    
-    public Numero getNumero(){
-    
+
+    public Numero getNumero() {
+
         return this.numero;
-        
+
     }
-    
+
     //metodo return toString
-    
-    public String toString(){
-    
+    public String toString() {
+
         return naipe + "_" + numero;
-    
+
     }
-    
+
 }
--- src/main/java/br/edu/ifsp/jscopa/Deck.java
+++ src/main/java/br/edu/ifsp/jscopa/Deck.java
@@ -3,32 +3,30 @@ package br.edu.ifsp.jscopa;
 import java.util.Random;
 
 public class Deck {
-    
+
     private Carta[] cartas;
     private int qtdCartasDeck;
 
-    public Deck(){
+    public Deck() {
 
         cartas = new Carta[108];
 
     }
 
     // metodo inicializador do deck
-
     public void reset() {
 
         Carta.Naipe[] naipes = Carta.Naipe.values();
         qtdCartasDeck = 0;
 
         // adiciona todas as cartas (de todos os valores (1 a 10) e naipes) no deck  
-        
-        for(int i = 0; i < naipes.length-1; i++){
+        for (int i = 0; i < naipes.length - 1; i++) {
 
             Carta.Naipe naipe = naipes[i];
 
             cartas[qtdCartasDeck++] = new Carta(naipe, Carta.Numero.getNumero(0));
 
-            for(int j = 1; j < 10; j++){
+            for (int j = 1; j < 10; j++) {
 
                 cartas[qtdCartasDeck++] = new Carta(naipe, Carta.Numero.getNumero(j));
                 cartas[qtdCartasDeck++] = new Carta(naipe, Carta.Numero.getNumero(j));
@@ -49,15 +47,14 @@ public class Deck {
 
         Random aleatorio = new Random();
 
-        for(int i = 0; i < cartas.length; i++){
+        for (int i = 0; i < cartas.length; i++) {
 
             //pegar um index aleatorio do array ALÉM do index atual
             //trocar o elemento aleatorio pelo elemento atual
-
             int valorAleatorio = i + aleatorio.nextInt(n - 1);
             Carta cartaAleatoria = cartas[valorAleatorio];
             cartas[valorAleatorio] = cartas[i];
-            cartas[i] = cartaAleatoria;  
+            cartas[i] = cartaAleatoria;
 
         }
 
