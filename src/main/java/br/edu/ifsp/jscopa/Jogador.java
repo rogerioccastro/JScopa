65030b1b536bbecd5e79c857d46b20f14e8e0994
R2leyser <51343917+R2leyser@users.noreply.github.com>
2024-12-03 10:53:49 -0300

    adicionar classe jogador

---
+++ src/main/java/br/edu/ifsp/jscopa/Jogador.java
@@ -0,0 +1,29 @@
+package br.edu.ifsp.jscopa;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Jogador {
+    private ArrayList<Carta> mao;
+
+    Jogador(ArrayList<Carta> mao) {
+        this.mao = mao;
+    }
+
+    public ArrayList<Carta> getMao() {
+        return mao;
+    }
+
+    public void setMao(ArrayList<Carta> mao) {
+        this.mao = mao;
+    }
+
+    public void jogarCarta(Carta carta) {
+        mao.remove(carta);
+    }
+
+    public void comprarCarta(Carta carta) {
+        mao.add(carta);
+    }
+
+}
